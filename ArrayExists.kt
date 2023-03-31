cd614f017de7bd6f776c81ce3ed5649f6d29832e
Hardik Kathiriya <hardikkathiriya0@gmail.com>
2023-03-31 13:21:46 +0530

    Update ArrayExists.kt

--- ArrayExists.kt
+++ ArrayExists.kt
@@ -1,6 +1,6 @@
 package com.demo.scaler
 
-fun main() {
+fun main() {  
     /**
      * This is my own solution not copied from other sources.
      * Please paste this 2 functions code together into a kotlin playground and run as is.
@@ -10,13 +10,18 @@ fun main() {
     val array2 = arrayOf(9, 11, 9, 13, 9)
     // Answer: [9, 11, 13]
 
+    //  val array1 = arrayOf("goat", "fish", "deer", "goat", "bear")
+    // 	val array2 = arrayOf("ant", "bear", "goat")
+    //Answer: ["ant", "goat"]   //For String array: Change solve function params as per given comments
+
     solve(array1, array2)
 }
 
-fun solve(array1: Array<Int>, array2: Array<Int>) {
-    var missingElements = mutableListOf<Int>()
+fun solve(array1: Array<Int>, array2: Array<Int>) {  //For String array: solve(array1: Array<String>, array2: Array<String>)
+    var missingElements = mutableListOf<Int>()  //For String array: var missingElements = mutableListOf<String>()
 
     //Map for repeating numbers
+    //For String array: val elementCountMap: MutableMap<String, Int> = array1.groupingBy { it }.eachCount().toMutableMap()
     val elementCountMap: MutableMap<Int, Int> = array1.groupingBy { it }.eachCount().toMutableMap()
 
     array2.forEach { it ->

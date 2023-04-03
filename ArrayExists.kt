ed002df7a081bd51447c0e828dfba4f2be204ec6
Hardik Kathiriya <hardikkathiriya0@gmail.com>
2023-04-03 13:16:37 +0530

    Update ArrayExists.kt

--- ArrayExists.kt
+++ ArrayExists.kt
@@ -1,6 +1,10 @@
 package com.demo.scaler
 
-fun main() {  
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
     /**
      * This is my own solution not copied from other sources.
      * Please paste this 2 functions code together into a kotlin playground and run as is.
@@ -10,19 +14,21 @@ fun main() {
     val array2 = arrayOf(9, 11, 9, 13, 9)
     // Answer: [9, 11, 13]
 
-    //  val array1 = arrayOf("goat", "fish", "deer", "goat", "bear")
-    // 	val array2 = arrayOf("ant", "bear", "goat")
-    //Answer: ["ant", "goat"]   //For String array: Change solve function params as per given comments
+//    UnComment this Code for String Array Check
+//    val array1 = arrayOf("goat", "fish", "deer", "goat", "bear")
+//    val array2 = arrayOf("ant", "bear", "goat")
+    //Answer: ["ant", "goat"]
 
-    solve(array1, array2)
+    val stringArray1 = array1.map { it.toString() }.toTypedArray()
+    val stringArray2 = array2.map { it.toString() }.toTypedArray()
+    solve(stringArray1, stringArray2, array1.firstOrNull()?.getType())
 }
 
-fun solve(array1: Array<Int>, array2: Array<Int>) {  //For String array: solve(array1: Array<String>, array2: Array<String>)
-    var missingElements = mutableListOf<Int>()  //For String array: var missingElements = mutableListOf<String>()
+fun solve(array1: Array<String>, array2: Array<String>, isType: VariableType?) {
+    var missingElements = mutableListOf<String>()
 
     //Map for repeating numbers
-    //For String array: val elementCountMap: MutableMap<String, Int> = array1.groupingBy { it }.eachCount().toMutableMap()
-    val elementCountMap: MutableMap<Int, Int> = array1.groupingBy { it }.eachCount().toMutableMap()
+    val elementCountMap: MutableMap<String, Int> = array1.groupingBy { it }.eachCount().toMutableMap()
 
     array2.forEach { it ->
         if (elementCountMap.containsKey(it)) {
@@ -39,6 +45,24 @@ fun solve(array1: Array<Int>, array2: Array<Int>) {  //For String array: solve(a
         }
     }
 
-    missingElements.sort()
-    println(missingElements)   // prints: [9, 11, 13]
+    if (isType == VariableType.INT) {
+        var missing = missingElements.map { it.toInt() }.toMutableList()
+        missing.sort()
+        println(missing)   // prints: [9, 11, 13], ["ant", "goat"]
+    } else {
+        missingElements.sort()
+        println(missingElements)   // prints: [9, 11, 13], ["ant", "goat"] 
+    }
+}
+
+fun Any.getType(): VariableType {
+    return when (this) {
+        is Int -> VariableType.INT
+        is String -> VariableType.STRING
+        else -> VariableType.UNKNOWN
+    }
+}
+
+enum class VariableType {
+    INT, STRING, PLAYER, UNKNOWN
 }
